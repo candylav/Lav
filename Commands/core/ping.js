8c7ec80834d6225ed9e853f1e5678ef7f8948862
candylav <maryambenallal@gmail.com>
2025-07-25 14:19:48 -0400

    feat: all commands migrated to SlashCommandBuilder and fixed

--- Commands/core/ping.js
+++ Commands/core/ping.js
@@ -1,12 +1,18 @@
+const { SlashCommandBuilder } = require('discord.js');
 const ms = require('ms');
 const { Translate } = require('../../process_tools');
 
 module.exports = {
-    name: 'ping',
-    description:("Get the ping of the bot!"),
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Get the ping of the bot!'),
 
-    async execute({ client, inter }) {
-        await inter.editReply("Ping?");
-        inter.editReply(await Translate(`Pong! API Latency is <${Math.round(client.ws.ping)}ms 🛰️>, last heartbeat calculated <${ms(Date.now() - client.ws.shards.first().lastPingTimestamp, { long: true })}> ago`));
+    async execute(interaction) {
+        await interaction.deferReply();
+
+        const ping = Math.round(interaction.client.ws.ping);
+        const heartbeatAgo = ms(Date.now() - interaction.client.ws.shards.first().lastPingTimestamp, { long: true });
+
+        return interaction.editReply(await Translate(`Pong! API Latency is <${ping}ms 🛰️>, last heartbeat calculated <${heartbeatAgo}> ago`));
     }
 };
